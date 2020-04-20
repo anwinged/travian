87d13eacb6ec67e2dc78ed40ff3459a7ea564969
Anton Vakhrushev <anwinged@ya.ru>
2020-04-20 22:30:44 +0300

    Fix pushing new build tasks

--- src/Scheduler.ts
+++ src/Scheduler.ts
@@ -199,7 +199,7 @@ function calculateInsertTime(tasks: ImmutableTaskList, name: string, args: Args,
 
     if (villageId && !insertedTs) {
         for (let taskTypePred of TASK_TYPE_PREDICATES) {
-            const sameVillageAndTypePred = t => sameVillage(villageId, t.args) && taskTypePred(t.name);
+            const sameVillageAndTypePred = t => sameVillage(villageId, t.args) && taskTypePred(name);
             insertedTs = lastTaskTime(tasks, sameVillageAndTypePred);
             if (insertedTs) {
                 insertedTs += 1;
