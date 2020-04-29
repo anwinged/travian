b9d6ebc45d56ca7781c6a4bb523867f7540efbe0
Anton Vakhrushev <anwinged@ya.ru>
2020-04-29 18:53:41 +0300

    Add error handling in index

--- src/index.js
+++ src/index.js
@@ -5,20 +5,28 @@ import { Executor } from './Executor';
 import { ControlPanel } from './ControlPanel';
 import TxtVersion from '!!raw-loader!./version.txt';
 
-const logger = new ConsoleLogger('Travian');
+function main() {
+    const logger = new ConsoleLogger('Travian');
 
-logger.log('TRAVIAN AUTOMATION', TxtVersion);
+    logger.log('TRAVIAN AUTOMATION', TxtVersion);
 
-const modeDetector = new ModeDetector();
-const scheduler = new Scheduler();
+    const modeDetector = new ModeDetector();
+    const scheduler = new Scheduler();
 
-if (modeDetector.isAuto()) {
-    modeDetector.setAuto();
-    logger.log('AUTO MANAGEMENT ON');
-    const executor = new Executor(TxtVersion, scheduler);
-    executor.run();
-} else {
-    logger.log('NORMAL MODE');
-    const controlPanel = new ControlPanel(TxtVersion, scheduler);
-    controlPanel.run();
+    if (modeDetector.isAuto()) {
+        modeDetector.setAuto();
+        logger.log('AUTO MANAGEMENT ON');
+        const executor = new Executor(TxtVersion, scheduler);
+        executor.run();
+    } else {
+        logger.log('NORMAL MODE');
+        const controlPanel = new ControlPanel(TxtVersion, scheduler);
+        controlPanel.run();
+    }
+}
+
+try {
+    main();
+} catch (e) {
+    setTimeout(() => location.reload(), 5000);
 }
